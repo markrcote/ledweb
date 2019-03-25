f1ef4c9d832dd94069be75ecf20e7a2e0c662c82
Mark Cote <mcote@mozilla.com>
2019-03-24 21:02:22 -0400

    Add comments to options.

--- ledservice.py
+++ ledservice.py
@@ -14,8 +14,15 @@ options.rows = 32
 options.cols = 64
 options.chain_length = 1
 options.parallel = 1
+
+# Use 'adafruit-hat' if you haven't soldered GPIO pins 4 and 18 together
+# (see https://github.com/hzeller/rpi-rgb-led-matrix#switch-the-pinout).
 options.hardware_mapping = 'adafruit-hat-pwm'
+
+# Newer Raspberry PIs put out data too quickly, which causes flickering.
+# This slows them down.
 options.gpio_slowdown = 2
+
 matrix = RGBMatrix(options=options)
 
 
