1234d3ae15ea81029cfc8790f821aa768d8ad74d
Mark Côté <mark.cote@uwaterloo.ca>
2019-05-07 22:08:06 -0400

    Clock mode.
    The command "clock", when sent to ledservice.py via redis, will
    put the service into clock mode.  This is a loop that will
    display the time to within about 1 ms.

---
+++ fonts/README.md
@@ -0,0 +1,3 @@
+This directory contains public-domain fonts taken from 
+https://github.com/hzeller/rpi-rgb-led-matrix/tree/master/fonts.  The original
+README is preserved here as well.
--- ledservice.py
+++ ledservice.py
@@ -6,7 +6,7 @@ import time
 import redis
 from PIL import Image
 
-from rgbmatrix import RGBMatrix
+from rgbmatrix import RGBMatrix, graphics
 
 from options import matrix_options
 
@@ -31,6 +31,40 @@ def display_png(img_filename):
     return 'success'
 
 
+def clock_loop(cli):
+    # Adapted from https://github.com/hzeller/rpi-rgb-led-matrix/blob/master/examples-api-use/clock.cc.
+    offscreen = matrix.CreateFrameCanvas()
+    font = graphics.Font()
+    font.LoadFont('fonts/5x7.bdf')
+    text_colour = graphics.Color(0, 255, 255)
+
+    next_time = time.time()
+
+    while cli.llen('matrix') == 0:
+        t = time.localtime(next_time)
+        ts = time.strftime('%I:%M %p', t)
+        if ts[0].startswith('0'):
+            ts = ' {}'.format(ts[1:])
+
+        offscreen.Clear()
+        graphics.DrawText(
+            offscreen,
+            font,
+            0,
+            font.baseline + 2,
+            text_colour,
+            ts
+        )
+
+        while time.time() < next_time:
+            time.sleep(0.001)
+
+        offscreen = matrix.SwapOnVSync(offscreen)
+        next_time += 1
+
+    clear_matrix()
+
+
 def loop():
     cli = redis.from_url('redis://localhost:6379')
     while True:
@@ -47,6 +81,8 @@ def loop():
         elif cmd[0] == 'display':
             print('displaying {}'.format(cmd[1]))
             display_png(cmd[1])
+        elif cmd[0] == 'clock':
+            clock_loop(cli)
         else:
             print('unknown command')
 
